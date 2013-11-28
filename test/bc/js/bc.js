2b6919bd15ff2e1f82c58d0b588fca312f1d1956
孙茂胤 <simonmysun@gmail.com>
2013-11-29 00:59:32 +0800

    test

--- test/bc/js/bc.js
+++ test/bc/js/bc.js
@@ -1,5 +1,34 @@
 var scrollPercent;
 
+var showComment = function(c) {
+    $(c).parent().parent().fadeIn();
+}
+
+var hideComment = function(c) {
+    $(c).parent().parent().fadeOut();
+}
+
+var bullet = function(b) {
+    //$(b).addClass("bullet");
+    //$("#bc-content").after($(b).clone);
+    //$(b).removeClass("bullet");
+    //$(".bullet").fadeOut();
+}
+
+var testBullet = function() {
+    $(".uyan_cmt_txt").each(function(){
+	var cmt_loc = parseFloat($(this).html());
+	if(cmt_loc >= (scrollPercent - halfPagePercent) && cmt_loc <= (scrollPercent + halfPagePercent)) {
+	    showComment(this);
+	    bullet(this);
+	}
+	else {
+	    hideComment(this);
+	}
+	return true;
+    });
+}
+
 $(window).scroll(function(){
     var s = $(window).scrollTop(),
     d = $(document).height(),
@@ -9,21 +38,12 @@ $(window).scroll(function(){
 
     halfPagePercent = (c / d) * 50;
 
-    $(".uyan_cmt_txt").each(function(){
-	var cmt_loc = parseFloat($(this).html());
-	if(cmt_loc >= (scrollPercent - halfPagePercent) && cmt_loc <= (scrollPercent + halfPagePercent)) {
-	    $(this).parent().parent().fadeIn();
-	    $(this).parent().parent().find("*").show();
-	}
-	else {
-	    $(this).parent().parent().fadeOut();
-	}
-	return true;
-    });
+    testBullet();
+
 
     //console.log("Current scroll percent range: [", (scrollPercent - halfPagePercent), (scrollPercent + halfPagePercent)),"]";
+    
+    $("#uyan_cmt_btn").attr("onclick",'$("#uyan_comment")[0].value=scrollPercent+"L_"+$("#uyan_comment")[0].value;UYAN.addCmt(this);');
+
 
 });
-
-
-$("#uyan_cmt_btn").attr("onclick",'$("#uyan_comment")[0].value=scrollPercent+"L_"+$("#uyan_comment")[0].value;UYAN.addCmt(this);');
