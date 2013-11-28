6ca7ebcc9772b7f4aaefd2009bb48af7789bfa43
孙茂胤 <simonmysun@gmail.com>
2013-11-28 17:56:53 +0800

    bind scroll percentage

--- test/bc/js/bc.js
+++ test/bc/js/bc.js
@@ -3,6 +3,6 @@ $(window).scroll(function(){
     d = $(document).height(),
     c = $(window).height();
     scrollPercent = (s / (d-c)) * 100;
-    console.log("Current scroll percent: " + scrollPercent);
-    $("#bc-post").append("Current scroll percent: " + scrollPercent+ "<br>");
+    //console.log("Current scroll percent: " + scrollPercent);
+    //$("#bc-post").append("Current scroll percent: " + scrollPercent+ "<br>");
 });
