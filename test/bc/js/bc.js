4219c5be7a9d57c3dbd77956ac4c31f2ae04978b
孙茂胤 <simonmysun@gmail.com>
2013-11-28 19:07:47 +0800

    show comment when correct

--- test/bc/js/bc.js
+++ test/bc/js/bc.js
@@ -2,7 +2,24 @@ $(window).scroll(function(){
     var s = $(window).scrollTop(),
     d = $(document).height(),
     c = $(window).height();
-    scrollPercent = (s / (d-c)) * 100;
-    //console.log("Current scroll percent: " + scrollPercent);
-    //$("#bc-post").append("Current scroll percent: " + scrollPercent+ "<br>");
+    
+    scrollPercent = (s / (d - c)) * 100;
+
+    halfPagePercent = (c / d) * 50;
+
+    $(".uyan_cmt_txt").each(function(){
+	var cmt_loc = parseFloat($(this).html());
+	if(cmt_loc >= (scrollPercent - halfPagePercent) && cmt_loc <= (scrollPercent + halfPagePercent)) {
+	    $(this).parent().parent().show();
+	}
+	else {
+	    $(this).parent().parent().hide();
+	}
+	return true;
+    });
+
+    //console.log("Current scroll percent range: [", (scrollPercent - halfPagePercent), (scrollPercent + halfPagePercent)),"]";
+
 });
+
+$("#uyan_cmt_list").children().each(function(){$(this).hide();});
