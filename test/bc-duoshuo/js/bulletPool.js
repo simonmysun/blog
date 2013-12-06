38caafa1dfff4c0ad08b7af44ddcfcc48136a035
孙茂胤 <simonmysun@gmail.com>
2013-12-06 21:47:41 +0800

    fuck

--- test/bc-duoshuo/js/bulletPool.js
+++ test/bc-duoshuo/js/bulletPool.js
@@ -5,8 +5,10 @@ var bulletPool = {
     createNew: function(id) {
 	var bp = {};
 
-	//bp.canvas = '#' + id;
-	bp.canvas = window;
+	bp.canvas = '#' + id;
+	//bp.canvas = window;
+	bp.fps = 10;
+	bp.speed = 1;
 
 	bp.init = function() {
 	    bp.changeSize();
@@ -34,12 +36,12 @@ var bulletPool = {
 	bp.changeSize = function() {
 	    bp.size.width = $(bp.canvas).width();
 	    bp.size.height = $(bp.canvas).height();
-	    bp.size.top = $(bp.canvas).scrollTop();
+	    bp.size.top = $(window).scrollTop();
 	    bp.size.maxRow = Math.floor(bp.size.width / 20) - 1;
 	};
 
 	bp.getRow = function() {
-	    for(x in bp.row) {
+	    for(x=0;x<bp.size.maxRow;x++) {
 		if(bp.row[x] <= 0) {
 		    return x;
 		}
@@ -49,21 +51,23 @@ var bulletPool = {
 
 	bp.add = function() {
 	    $(".ds-post").each(function() {
-		var bullet = new Object();
-		bullet.id = $(this).attr("data-post-id") + "";
-		bullet.loc = parseFloat($(this).find("p").html());
-		bullet.content = $(this).find("p").html().replace(/.*L_/,"");
-		bullet.flying = false;
-		bp.bulletList[$(this).attr("data-post-id")] = bullet;
+		if(bp.bulletList[$(this).attr("data-post-id")] == undefined) {
+		    var bullet = new Object();
+		    bullet.id = $(this).attr("data-post-id") + "";
+		    bullet.loc = parseFloat($(this).find("p").html());
+		    bullet.content = $(this).find("p").html().replace(/.*L_/,"");
+		    bullet.flying = false;
+		    bp.bulletList[$(this).attr("data-post-id")] = bullet;
+		}
 		return true;
 	    });
 	};
 
 	bp.fly = function(x,loc) {
 	    console.log("fly x");
-	    bp.flying[x] = 1;
-	    $("#bc-content").append('<div class="bullet" id="' + x + '" style="left:' + bp.size.width + ';top:' + (bp.size.top + loc * 20) + 'px;">' + bp.bulletList[x].content + '</div>'); 
-	    bp.row[loc] = $("#" + x).width();
+	    bp.flying[x] = loc;
+	    $("#bc-content").append('<div class="bullet" id="' + x + '" style="left:' + bp.size.width + 'px;top:' + (bp.size.top + loc * 20) + 'px;">' + bp.bulletList[x].content + '</div>'); 
+	    bp.row[loc] = $("#" + x)[0].offsetWidth;
 	    bp.bulletList[x].flying = true;
 	}
 
@@ -73,25 +77,29 @@ var bulletPool = {
 	    while(loc != -1 && flag == 1) {
 		flag = 0;
 		for(x in bp.wait) {
-		    bp.fly(x,loc);
-		    bp.wait[x] = undefined;
-		    flag = 1;
-		    break;
-		}
-		loc = bp.getRow();
-	    }
-	    flag = 1;
-	    while(loc != -1 && flag == 1) {
-		flag = 0;
-		for(x in bp.bulletList) {
-		    if(bp.bulletList[x].flying == false && in_(bp.bulletList[x].loc, (scrollPercent() - halfPagePercent()), (scrollPercent() + halfPagePercent()))) {
+		    if(bp.wait[x]!=undefined) {
 			bp.fly(x,loc);
+			bp.wait[x] = undefined;
 			flag = 1;
 			break;
 		    }
-		    if(flag == 0) {
-			bp.wait[x] = 1;
+		}
+		loc = bp.getRow();
+	    }
+	    flag = 0;
+	    while(loc != -1 && flag >= 0) {
+		flag = -1;
+		for(x in bp.bulletList) {
+		    if(bp.bulletList[x]!=undefined){
+			if(bp.bulletList[x].flying == false && in_(bp.bulletList[x].loc, (scrollPercent() - halfPagePercent()), (scrollPercent() + halfPagePercent()))) {
+			    bp.fly(x,loc);
+			    flag = 2;
+			    break;
+			}
 		    }
+		}
+		if(flag == 1) {
+		    bp.wait[x] = 1;
 		}
 		loc = bp.getRow();
 	    }
@@ -99,32 +107,34 @@ var bulletPool = {
 
 	bp.refresh = function() {
 	    
+	    console.log("draw");
+	    
 	    bp.changeSize();
 	    bp.add();
 	    bp.launch();
 	    
-	    $('.bullet')
-		.css('left',function(index,left) {
-		    return left - speed;
-		})
-		.each(function(index,bullet) {
-		    console.log("fly");
-		    var left =parseInt(bullet.style.left); 
-		    var offset =parseInt(bullet.offsetWidth);
-		    if (offset <=10) {
-			bp.bulletList[bullet.attr('id')],flying = false;
-			bp.flying[bullet.attr('id')] = undefined;
-		    }
-		});
+	    $('.bullet').css('left',function(index,left) {
+		return (parseInt(left) - bp.speed) + 'px';
+	    });
+	    $('.bullet').css('top',function(){return((bp.size.top + (bp.flying[$(this).attr('id')])) + 'px');});
+	    $('.bullet').each(function(index,bullet) {
+		console.log("fly");
+		var left =parseInt(bullet.style.left); 
+		var offset =parseInt(bullet.offsetWidth);
+		if (left + offset < 5) {
+		    bp.bulletList[$(bullet).attr('id')].flying = false;
+		    bp.flying[$(bullet).attr('id')] = undefined;
+		}
+	    });
 	    for(x in bp.row) {
 		if(bp.row[x] > 0) {
-		    bp.row[x] -= speed;
+		    bp.row[x] -= bp.speed;
 		}
 	    }
 
 	    setTimeout(function() {
 		bp.refresh();
-	    },100);
+	    },bp.fps);
 	};
 	
 	return bp;
